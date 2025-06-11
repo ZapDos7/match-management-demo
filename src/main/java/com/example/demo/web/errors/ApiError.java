410dcbf3a57d05562d3fb0bd6ce349ca69eb99bb
ZapDos7 <jean.darker007@gmail.com>
2025-06-12 00:26:42 +0300

    validation exception handling

--- src/main/java/com/example/demo/web/errors/ApiError.java
+++ src/main/java/com/example/demo/web/errors/ApiError.java
@@ -3,17 +3,20 @@ package com.example.demo.web.errors;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class ApiError {
     private int status;
     private String message;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
+    private List<String> errors;
 
-    public ApiError(int status, String message) {
+    public ApiError(int status, String message, List<String> errors) {
         this.status = status;
         this.message = message;
         this.timestamp = LocalDateTime.now();
+        this.errors = errors;
     }
 
     public int getStatus() {
@@ -38,5 +41,13 @@ public class ApiError {
 
     public void setTimestamp(LocalDateTime timestamp) {
         this.timestamp = timestamp;
+    }
+
+    public List<String> getErrors() {
+        return errors;
+    }
+
+    public void setErrors(List<String> errors) {
+        this.errors = errors;
     }
 }
